ae5b4b3856258303f232160483fbc57476a3a7d6
rcoscali <rcoscali>
2000-08-07 01:24:45 +0000

    Removed a glShadeModel(mooth) forgotten after a test

--- freewrl/VRMLRend.pm
+++ freewrl/VRMLRend.pm
@@ -20,6 +20,9 @@
 #                      %RendC, %PrepC, %FinC, %ChildC, %LightC
 #
 # $Log$
+# Revision 1.9  2000/08/07 01:24:45  rcoscali
+# Removed a glShadeModel(mooth) forgotten after a test
+#
 # Revision 1.8  2000/08/07 01:03:09  rcoscali
 # Fixed another little problem on texture of cone. It was mapped in the wrong way.
 #
@@ -293,7 +296,6 @@ Sphere => 'int vdiv = vert_div;
 		       /* glPushAttrib(GL_LIGHTING); This does what? */
 			   /* glShadeModel(GL_SMOOTH); This makes a smoother shading */
 		}
-glShadeModel(GL_SMOOTH);
 		glScalef($f(radius), $f(radius), $f(radius));
 		glBegin(GL_QUAD_STRIP);
 		START_TRIG1
