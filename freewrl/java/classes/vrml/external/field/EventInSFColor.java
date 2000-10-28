85fc2a5627311031dab534fd20665eb751d17bec
crc_canada <crc_canada>
2000-10-28 19:16:50 +0000

    0.27 EAI

--- freewrl/java/classes/vrml/external/field/EventInSFColor.java
+++ freewrl/java/classes/vrml/external/field/EventInSFColor.java
@@ -1,20 +1,16 @@
 package vrml.external.field;
 import vrml.external.field.FieldTypes;
-import vrml.external.Node;
 import vrml.external.Browser;
 
 public class EventInSFColor extends EventIn {
 
   public EventInSFColor() { EventType = FieldTypes.SFCOLOR; }
 
-  public void          setValue() {
-    System.out.println ("ERROR: eventinSFColor Not Implemented");
+  public void          setValue(float[] value) throws
+	IllegalArgumentException {
+
+        Browser.SendEvent (inNode , command, "" + value[0] + " " + value[1] +
+                  " " + value[2]);
     return;
-  }
-
-  public void          set1Value(int index) {
-    System.out.println ("ERROR: eventinSFColor.set1vanue Not Implemented");
-
-  return;
   }
 }
