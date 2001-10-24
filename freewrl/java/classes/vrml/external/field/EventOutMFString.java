ca9268c34102170b845847c108de6f4451e2eae7
crc_canada <crc_canada>
2001-10-24 14:24:07 +0000

    Now works!

--- freewrl/java/classes/vrml/external/field/EventOutMFString.java
+++ freewrl/java/classes/vrml/external/field/EventOutMFString.java
@@ -5,34 +5,52 @@ import vrml.external.Browser;
 
 
 public class EventOutMFString extends EventOutMField {
+
+  // retstr is an array of string values.
+  // sizeof is the size of retstr.
+
+  String[] retstr;
+  int sizeof = 0;
+
+
   public EventOutMFString() {EventType = FieldTypes.MFSTRING;}
 
   public String[]      getValue() {
     String rep;
-    int count;
     StringTokenizer tokens;
 
+    // System.out.println ("DEBUG - EventOutMFString");
     if (command != null) {
+      // System.out.println ("DEBUG - EventOutMFString - command not equals null " + command);
       rep = Browser.SendEventOut (outNode, command);
       tokens = new StringTokenizer (rep,"\"");
     } else {
       tokens = new StringTokenizer (RLreturn,"\"");
     }
-    String[] retstr = new String[(tokens.countTokens()/2)];
-    count = 0;
+    retstr = new String[(tokens.countTokens()/2)];
+    sizeof = 0;
 
     rep = tokens.nextToken();
     while (tokens.hasMoreTokens()) {
-      retstr[count] = tokens.nextToken();
+      retstr[sizeof] = tokens.nextToken();
       if (tokens.hasMoreTokens()) rep = tokens.nextToken();
-      count ++;
+      sizeof ++;
     }
+    //sizeof = sizeof-1;
+
     return retstr;
   }
 
   public String        get1Value(int index) {
-    String[] retstr = getValue();
-    System.out.println ("ERROR:MFString, get1value, whole string is " + retstr);
+    if ((index > sizeof) || (index < 0)) {
+	System.out.println ("EventOutMFString.get1Value - index " + index +
+		" out of range");
+	index = 0;
+    }
     return retstr[index];
+  }
+
+  public int           getSize() {
+	return sizeof;
   }
 }
