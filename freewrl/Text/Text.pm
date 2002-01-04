2dc08f24292e86c96975cae801480e3ddc80b7f0
crc_canada <crc_canada>
2002-01-04 20:13:31 +0000

    Freetype v2 conversion. Now, this passes only fonts into the Text module; actual loading of fonts is done on demand.

--- freewrl/Text/Text.pm
+++ freewrl/Text/Text.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 1998 Tuomas J. Lukka
+# Copyright (C) 1998 Tuomas J. Lukka, (C)2001 John Stewart CRC Canada.
 # DISTRIBUTED WITH NO WARRANTY, EXPRESS OR IMPLIED.
 # See the GNU Library General Public License (file COPYING in the distribution)
 # for conditions of use and redistribution.
@@ -8,6 +8,11 @@ require DynaLoader;
 @ISA=DynaLoader;
 bootstrap VRML::Text;
 
-open_font(($VRML::ENV{FREEWRL_FONTS}||"fonts")."/baklava.ttf");
+
+# just tell Text.xs where to find the fonts. If the first path (first param)
+# can not be found, then the second should have the baklava font supplied
+# with FreeWRL
+
+open_font($VRML::ENV{FREETYPE_FONT_PATH},($VRML::ENV{FREEWRL_FONTS}||"fonts"));
 
 1;
