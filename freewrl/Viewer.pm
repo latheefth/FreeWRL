dfcb01ad7dc49137d354115a422c6586a78ce2d7
crc_canada <crc_canada>
2002-01-30 17:06:22 +0000

    Viewer "NONE" was broken.

--- freewrl/Viewer.pm
+++ freewrl/Viewer.pm
@@ -121,13 +121,8 @@ sub togl {
 package VRML::Viewer::None;
 @VRML::Viewer::None::ISA=VRML::Viewer;
 
-sub new {
-	my($type, $loc, $ori) = @_;
-	my $this = VRML::Viewer->new();
-	$this->{Pos} = $loc;
-	$this->{Quat} = new_vrmlrot VRML::Quaternion(@$ori);
-	return $this;
-}
+sub handle {}
+
 
 package VRML::Viewer::Walk;
 @VRML::Viewer::Walk::ISA=VRML::Viewer;
