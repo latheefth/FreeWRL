35277afc5521907cfb5d3bedda933542bcb263ab
crc_canada <crc_canada>
2002-05-23 18:31:27 +0000

    Made different tess functions for Extrusion and IndexedFaceSet, and did some renaming.

--- freewrl/CFuncs/Tess.c
+++ freewrl/CFuncs/Tess.c
@@ -28,8 +28,8 @@
 #include "perl.h"
 #include "XSUB.h"
 
-#include <math.h>
 
+#include <math.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glx.h>
@@ -40,12 +40,14 @@
   
 GLUtriangulatorObj *global_tessobj;	
 struct VRML_PolyRep *global_tess_polyrep=NULL;
+int global_IFS_Coords[500];
+int global_IFS_Coord_count=0;
 
 /* and now all the callback functions, which will be called
 	by OpenGL automatically, if the Polygon is specified	*/
 
-void ber_tess_begin(GLenum e) {
-               //printf(" ber_tess_begin   e = %s\\n", (e == GL_TRIANGLES ? "GL_TRIANGLES" : "UNKNOWN")); 
+void FW_tess_begin(GLenum e) {
+               /* printf(" FW_tess_begin   e = %s\n", (e == GL_TRIANGLES ? "GL_TRIANGLES" : "UNKNOWN"));  */
 		/* we only should get GL_TRIANGLES as type, because
 		we defined  the edge_flag callback		*/
 		/* check, if the structure is there		*/
@@ -53,30 +55,37 @@ void ber_tess_begin(GLenum e) {
 		die("Something went wrong while tessellating!");
 }
 
-void ber_tess_end(void) {
-	//printf("ber_tess_end: Tesselation done.\\n");
+void FW_tess_end(void) {
+	/* printf("FW_tess_end: Tesselation done.\n"); */
 	/* nothing to do	*/
 }
 
-void ber_tess_edgeflag(GLenum flag) {
-	// printf("ber_tess_end: An edge was done (flag = %d).\\n", flag); 
+void FW_tess_edgeflag(GLenum flag) {
+	/* printf("FW_tess_edgeflag: An edge was done (flag = %d).\n", flag); */
 	/* nothing to do, this function has to be registered
 	so that only GL_TRIANGLES are used	*/
 }
 
-void ber_tess_vertex(void *p) {
+void FW_IFS_tess_vertex(void *p) {
+	int *dp=p;
+
+	global_IFS_Coords[global_IFS_Coord_count++] = *dp;
+	printf ("tess vertex, %d count now is %d\n",*dp,global_IFS_Coord_count);
+
+}
+
+void FW_Extrusion_tess_vertex(void *p) {
 	static int x=0;
 	GLdouble *dp=p;
 
-#define GTP global_tess_polyrep
-	
-	if(GTP->ntri >= GTP->alloc_tri) {
+	if(global_tess_polyrep->ntri >= global_tess_polyrep->alloc_tri) {
 		die("Too many tesselated triangles!");
 	}
 
-	//printf("ber_tess_vertex: A vertex is coming   x = %d   ntri = %d.\\n", x, GTP->ntri); 
-#undef GTP
+	/* printf ("FW_tess_vertex, %f %f %f\n",dp[0],dp[1],dp[2]);*/
+	/* printf("FW_tess_vertex: A vertex is coming   x = %d   ntri = %d.\n", x, global_tess_polyrep->ntri); */
 	
+
 	global_tess_polyrep->coord[(global_tess_polyrep->ntri)*9+x*3]  =dp[0];
 	global_tess_polyrep->coord[(global_tess_polyrep->ntri)*9+x*3+1]=dp[1];
 	global_tess_polyrep->coord[(global_tess_polyrep->ntri)*9+x*3+2]=dp[2];
@@ -86,11 +95,22 @@ void ber_tess_vertex(void *p) {
 		x=0;
 		(global_tess_polyrep->ntri)++;
 	} else x++;
+
+
 }
 
-void ber_tess_error(GLenum e) {
-	printf("ERROR %d: >%s<\n",e,gluErrorString(e));
+void FW_tess_error(GLenum e) {
+	/* printf("ERROR %d: >%s<\n",e,gluErrorString(e));*/
 }
+
+void tesselize_ifs() {
+	gluTessCallback(global_tessobj,GLU_VERTEX,FW_IFS_tess_vertex);
+}
+
+void tesselize_extrusion() {
+	gluTessCallback(global_tessobj,GLU_VERTEX,FW_Extrusion_tess_vertex);
+}
+	
 
 /* next function has to be called once, after an OpenGL context is made
 	and before tessellation is started			*/
@@ -101,13 +121,11 @@ void new_tessellation(void) {
 		die("Got no memory for Tessellation Object!");
 		
 	/* register the CallBackfunctions				*/
-	gluTessCallback(global_tessobj,GLU_BEGIN,ber_tess_begin);
-	gluTessCallback(global_tessobj,GLU_EDGE_FLAG,ber_tess_edgeflag);
-	gluTessCallback(global_tessobj,GLU_VERTEX,ber_tess_vertex);
-	gluTessCallback(global_tessobj,GLU_ERROR,ber_tess_error);
-	gluTessCallback(global_tessobj,GLU_END,ber_tess_end);
-	
-	//printf("Tessellation Initialized!\n");
+	gluTessCallback(global_tessobj,GLU_BEGIN,FW_tess_begin);
+	gluTessCallback(global_tessobj,GLU_EDGE_FLAG,FW_tess_edgeflag);
+	gluTessCallback(global_tessobj,GLU_VERTEX,FW_IFS_tess_vertex);
+	gluTessCallback(global_tessobj,GLU_ERROR,FW_tess_error);
+	gluTessCallback(global_tessobj,GLU_END,FW_tess_end);
 }
 
 /* next function should be called once at the end, but where?	*/
