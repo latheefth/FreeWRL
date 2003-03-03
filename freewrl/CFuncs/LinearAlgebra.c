2ceca48c2de2a1f92c0028901503497e5cc2e489
crc_canada <crc_canada>
2003-03-03 20:52:11 +0000

    Fixed bug in bounds checking code for acos() call - worked ok when param was close to or greater than +1, returned wrong result when param was close to -1.

--- freewrl/CFuncs/LinearAlgebra.c
+++ freewrl/CFuncs/LinearAlgebra.c
@@ -39,7 +39,7 @@ float calc_angle_between_two_vectors(struct pt a, struct pt b)
     length_a = calc_vector_length(a);
     length_b = calc_vector_length(b);
 
-    /*printf("scalar: %f  length_a: %f  length_b: %f \n", scalar, length_a, length_b);*/
+    printf("scalar: %f  length_a: %f  length_b: %f \n", scalar, length_a, length_b);
 	
     if (scalar == 0){
 	return M_PI/2;	
@@ -51,11 +51,13 @@ float calc_angle_between_two_vectors(struct pt a, struct pt b)
     }
 	
     temp = scalar /(length_a * length_b);
-    /*printf("temp: %f", temp);*/
+    // printf("temp: %f", temp);
 
     /*acos() appears to be unable to handle 1 and -1  */
+    /* fixed to handle border case where temp <=-1.0 for 0.39 JAS */
     if ((temp >= 1) || (temp <= -1)){
-	return 0;
+	if (temp < 0.0) return 3.141526;
+	return 0.0;
     }
     return acos(temp);
 }
