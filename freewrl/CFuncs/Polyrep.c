e9e35f61dd17b23f1072127142ee0fc2dab0d00c
crc_canada <crc_canada>
2003-04-03 14:21:40 +0000

    Previous version incorrect

--- freewrl/CFuncs/Polyrep.c
+++ freewrl/CFuncs/Polyrep.c
@@ -59,10 +59,11 @@ int count_IFS_faces(int cin, struct VRML_IndexedFaceSet *this_IFS) {
 	}
 
 		
+	/*
 	printf ("this structure has %d faces\n",faces);
 	printf ("	max points per face %d\n",max_points_per_face);
 	printf ("	min points per face %d\n\n",min_points_per_face);
-	
+	*/
 	
 	if (faces < 1) {
 		printf("an IndexedFaceSet with no faces found\n");
@@ -203,7 +204,6 @@ void IFS_face_normals (
 		if (i<faces-1) {
 			while (((this_IFS->coordIndex.p[tmp_a-1]) != -1) &&
 				(tmp_a < cin-2)) {
-				this_IFS->coordIndex.p[tmp_a-1], tmp_a);
 				tmp_a++;
 			}
 		}
