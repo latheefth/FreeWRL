74e15ff7593b4820c9eaf338f300987998bc8598
sdumoulin <sdumoulin>
2004-03-03 21:24:25 +0000

    # Interface files for AQUA

---
+++ freewrl/CFuncs/aquaInt.h
@@ -0,0 +1,12 @@
+//
+//  aquaInterface.h
+//  FreeWRL
+//
+//  Created by Sarah Dumoulin on Mon Jan 19 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+void updateContext();
+float getWidth();
+float getHeight();
+void  setAquaCursor(int ctype);
