17611696d15bc568726206d9b35b51a083041ad8
crc_canada <crc_canada>
2005-01-19 17:40:57 +0000

    syntax problem with ConsoleMessage call

--- freewrl/USE.pm
+++ freewrl/USE.pm
@@ -74,8 +74,8 @@ sub name {
 sub node {
 	my ($this) = @_;
 	if (!defined  ($this->{DEFNode})) {
-	    VRML::VRMLFunc::ConsoleMessage( "USE name: ", $this->{DEFName}, " not DEF'd\n".
-	    "Unrecoverable error; FreeWRL has to exit. \n");
+		my $msg = "USE name: ", $this->{DEFName}, " not DEF'd\n".  "Unrecoverable error; FreeWRL has to exit. \n";
+	    VRML::VRMLFunc::ConsoleMessage($msg); 
 	    exit(1);
 	}
 
