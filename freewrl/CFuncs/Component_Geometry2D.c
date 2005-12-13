00147ced52b55042e8a71a54c9b6b6ffa7fad33b
crc_canada <crc_canada>
2005-12-13 20:25:43 +0000

    Arc2D

--- freewrl/CFuncs/Component_Geometry2D.c
+++ freewrl/CFuncs/Component_Geometry2D.c
@@ -14,21 +14,72 @@
 #include "headers.h"
 
 #define SEGMENTS_PER_CIRCLE 36
+#define PIE 10
+#define CHORD 20
+#define NONE 30
+
 void *createLines (float start, float end, float radius, int closed, int *size);
 
 void render_Arc2D (struct VRML_Arc2D *this_) {
+	STRLEN xx;
+
         if (this_->_ichange != this_->_change) {
                 /*  have to regen the shape*/
                 this_->_ichange = this_->_change;
 		
-		printf ("render_Arc2D, endAngle %f startAngle %f radius %f\n",
-			this_->endAngle, this_->startAngle, this_->radius);
-
 		FREE_IF_NZ (this_->__points);
 		this_->__numPoints = 0;
 		this_->__points = createLines (this_->startAngle,
-			this_->endAngle, this_->radius, FALSE, &this_->__numPoints);
+			this_->endAngle, this_->radius, NONE, &this_->__numPoints);
 	}
+
+	if (this_->__numPoints>0) {	
+                       glDisable(GL_CULL_FACE);
+
+		glDisableClientState (GL_NORMAL_ARRAY);
+		glVertexPointer (3,GL_FLOAT,0,(GLfloat *)this_->__points);
+        	glDrawArrays (GL_LINE_STRIP, 0, this_->__numPoints);
+		glEnableClientState (GL_NORMAL_ARRAY);
+		glEnable (GL_CULL_FACE);
+
+	}
+}
+
+
+
+void render_ArcClose2D (struct VRML_ArcClose2D *this_) {
+	STRLEN xx;
+	char *ct;
+
+        if (this_->_ichange != this_->_change) {
+                /*  have to regen the shape*/
+                this_->_ichange = this_->_change;
+		
+		FREE_IF_NZ (this_->__points);
+		this_->__numPoints = 0;
+
+		ct = SvPV(this_->closureType,xx);
+printf ("ct %s, len %d\n",ct,xx);
+
+		if (strncmp(ct,"PIE",xx) == 0) {
+			this_->__points = createLines (this_->startAngle,
+				this_->endAngle, this_->radius, PIE, &this_->__numPoints);
+		} else if (strncmp(ct,"CHORD",xx) == 0) {
+			this_->__points = createLines (this_->startAngle,
+				this_->endAngle, this_->radius, CHORD, &this_->__numPoints);
+		} else {
+			printf ("ArcClose2D, closureType %s invalid\n",this_->closureType);
+		}
+	}
+
+printf ("mumPotits %d\n",this_->__numPoints);
+
+        glPushAttrib(GL_ENABLE_BIT);
+        glDisable (GL_LIGHTING);
+        glDisable(GL_COLOR_MATERIAL);
+        glDisable(GL_CULL_FACE);
+
+
 
 	if (this_->__numPoints>0) {	
 		glDisableClientState (GL_NORMAL_ARRAY);
@@ -36,9 +87,10 @@ void render_Arc2D (struct VRML_Arc2D *this_) {
         	glDrawArrays (GL_LINE_STRIP, 0, this_->__numPoints);
 		glEnableClientState (GL_NORMAL_ARRAY);
 	}
+	glPopAttrib();
 }
 
-void render_ArcClose2D (struct VRML_ArcClose2D *this_){}
+
 void render_Circle2D (struct VRML_Circle2D *this_){}
 void render_Disk2D (struct VRML_Disk2D *this_){}
 void render_Polyline2D (struct VRML_Polyline2D *this_){}
@@ -82,14 +134,18 @@ void *createLines (float start, float end, float radius, int closed, int *size) 
 	/* we always have to draw the line - we have a line strip, and we calculate
 	   the beginning points; we have also to calculate the ending point. */
 	numPoints++;
+printf ("numPoints is %d\n",numPoints);
 
-	printf ("numPoints %d start-end %f\n",numPoints, start-end);
+	/* closure type */
+	if (closed == CHORD) numPoints++;
+	if (closed == PIE) numPoints+=2;
+
 
 	points = malloc (sizeof(float)*numPoints*3);
 	fp = points;
 
 	for (i=0; i<numPoints; i++) {
-		*fp = radius* sinf((PI * 2.0 * (float)i)/((float)SEGMENTS_PER_CIRCLE));	
+		*fp = -radius * sinf((PI * 2.0 * (float)i)/((float)SEGMENTS_PER_CIRCLE));	
 		fp++;
 		*fp = radius * cosf((PI * 2.0 * (float)i)/((float)SEGMENTS_PER_CIRCLE));	
 		fp++;
@@ -98,6 +154,34 @@ void *createLines (float start, float end, float radius, int closed, int *size) 
 	}
 
 	/* do we have to draw any pies, cords, etc, etc? */
+	if (closed == CHORD) {
+printf ("have closure type of CHORD, npoints now %d\n",numPoints);
+
+		/* loop back to origin */
+*fp = 10.0;
+/*
+		*fp = -radius * sinf(0.0/((float)SEGMENTS_PER_CIRCLE));	
+*/
+printf ("back to %f ",*fp);
+		fp++;
+		*fp = radius * cosf(0.0/((float)SEGMENTS_PER_CIRCLE));	
+printf (" %f\n");
+		fp++;
+		*fp=0.0; /* z is always zero */
+		fp++;
+	} else if (closed == PIE) {
+printf ("have closure type of PIE\n");
+
+		/* go to origin */
+		*fp = 0.0; fp++; *fp=0.0; fp++; *fp=0.0; fp++;
+
+		*fp = -radius * sinf(0.0/((float)SEGMENTS_PER_CIRCLE));	
+		fp++;
+		*fp = radius * cosf(0.0/((float)SEGMENTS_PER_CIRCLE));	
+		fp++;
+		*fp=0.0; /* z is always zero */
+		fp++;
+	}
 
 		
 	*size = numPoints;
