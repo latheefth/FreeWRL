4a0f89bf42fb8832f9125696dffe8ee070a9c9ad
crc_canada <crc_canada>
2006-03-09 20:44:58 +0000

    Initial Component Event Utilities

--- freewrl/CFuncs/headers.h
+++ freewrl/CFuncs/headers.h
@@ -890,6 +890,16 @@ void child_InlineLoadControl (struct X3D_InlineLoadControl *this_);
 void changed_InlineLoadControl (struct X3D_InlineLoadControl *this_);
 void changed_Anchor (struct X3D_Anchor *this_);
 
+/* Event Utilities Component */
+void do_BooleanFilter ( void *ptr, int ev, int over);
+void do_BooleanSequencer ( void *ptr, int ev, int over);
+void do_BooleanToggle ( void *ptr, int ev, int over);
+void do_BooleanTrigger ( void *ptr, int ev, int over);
+void do_IntegerSequencer ( void *ptr, int ev, int over);
+void do_IntegerTrigger ( void *ptr, int ev, int over);
+void do_TimeTrigger ( void *ptr, int ev, int over);
+
+
 
 #define NODE_ADD_PARENT(a) add_parent(a,ptr)
 #define NODE_REMOVE_PARENT(a) add_parent(a,ptr)
