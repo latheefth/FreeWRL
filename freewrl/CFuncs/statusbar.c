12973707f656b0b363fba82371d2fd0244af559a
crc_canada <crc_canada>
2006-03-29 21:34:23 +0000

    remove statusbar.c

--- freewrl/CFuncs/statusbar.c
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright(C) 1998 Tuomas J. Lukka, 2001, 2002 John Stewart. CRC Canada.
- * NO WARRANTY. See the license (the file COPYING in the VRML::Browser
- * distribution) for details.
- */
-
