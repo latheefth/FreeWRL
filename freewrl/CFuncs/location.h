4876896f5fa649876c33beb687dd3901e16e8c6b
crc_canada <crc_canada>
2007-10-09 17:13:57 +0000

    new files for MIDI inclusion for Linux only

---
+++ freewrl/CFuncs/location.h
@@ -0,0 +1,4 @@
+/* if this is in the MIDI compile area, we define the following, if not (eg, in CFuncs, the 
+following should be UNDEFINED */
+
+#undef REWIRE
---
+++ freewrl/MIDI/reasonMIDIDevices.c
@@ -0,0 +1,21 @@
+/*******************************************************************
+ Copyright (C) 2007 John Stewart, CRC Canada.
+ DISTRIBUTED WITH NO WARRANTY, EXPRESS OR IMPLIED.
+ See the GNU Library General Public License (file COPYING in the distribution)
+ for conditions of use and redistribution.
+
+ Note that these files are stripped of comments, and non-Linux code. The reason for
+ this is that Propellerheads' ReWire IP is protected - the Linux code does not 
+ contain any ReWire protocol code (despite file names). At the present time, I am
+ unsure if the original source, with all comments and #ifdefs, and Propellerheads'
+ system calls can be released as open source.
+
+ John Stewart, CRC Canada.
+ October 8, 2007.
+
+*********************************************************************/
+# 1 "reasonMIDIDevices.c"
+# 1 "/root/usb/FreeWRL_Reason_MIDI_Interface//"
+# 1 "<built-in>"
+# 1 "<command line>"
+# 1 "reasonMIDIDevices.c"
