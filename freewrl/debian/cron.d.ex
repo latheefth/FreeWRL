90ae24d35960a61481d38cc3e1961b4945f946d0
couannette <couannette>
2007-11-03 18:06:50 +0000

    clean up

--- freewrl/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the freewrl package
-#
-0 4	* * *	root	freewrl_maintenance
--- freewrl/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(freewrl):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="freewrl" command="/usr/bin/freewrl"
