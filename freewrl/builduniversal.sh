8b5857e38cd6092e4896b0126d79a7a6e655c752
crc_canada <crc_canada>
2007-11-23 15:47:42 +0000

    split the Perl Makefile stuff so that it is for OSX and for Linux - it was getting too hairy having a configure-config system for both platforms.

--- freewrl/builduniversal.sh
+++ freewrl/builduniversal.sh
@@ -8,7 +8,7 @@ cp -f JS/js1.5/src/Makefile.ref.uni JS/js1.5/src/Makefile.ref
 cp -f JS/js1.5/src/Makefile.in.uni JS/js1.5/src/Makefile.in
 cp -f JS/js1.5/src/rules.mk.uni JS/js1.5/src/rules.mk
 cp -f vrml.conf.i386 vrml.conf.aqua
-perl Makefile.PL
+perl Makefile.PL.osx
 make install
 cp -f libFreeWRLFunc.dylib VRMLFunc_i386.dylib
 cp -f JS/js1.5/src/Darwin_OPT.OBJ/libFreeWRLjs.so libFreeWRLjs_i386.so
@@ -18,7 +18,7 @@ make distclean
 cp -f vrml.conf.ppc vrml.conf.aqua
 cp -f JS/js1.5/src/config/Darwin.mk.ppc JS/js1.5/src/config/Darwin.mk
 cp -f JS/js1.5/src/editline/Makefile.ref.ppc JS/js1.5/src/editline/Makefile.ref
-perl Makefile.PL
+perl Makefile.PL.osx
 
 # Have to change gcc version here, as MakeMaker doesn't let you pass it as an option?
 make install
