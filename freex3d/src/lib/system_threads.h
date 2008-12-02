93a1667182266fad87e6617ccff38de178162140
couannette <couannette>
2008-12-02 18:19:25 +0000

    Added some new files.

---
+++ freex3d/src/lib/system_threads.h
@@ -0,0 +1,20 @@
+/*
+=INSERT_TEMPLATE_HERE=
+
+$Id$
+
+FreeX3D support library.
+Internal header: threads dependencies.
+
+*/
+
+#ifndef __LIBFREEX3D_SYSTEM_THREADS_H__
+#define __LIBFREEX3D_SYSTEM_THREADS_H__
+
+
+#if HAVE_PTHREAD
+# include <pthread.h>
+#endif
+
+
+#endif /* __LIBFREEX3D_SYSTEM_THREADS_H__ */
---
+++ freex3d/src/lib/ui/ui.h
@@ -0,0 +1,17 @@
+/*
+=INSERT_TEMPLATE_HERE=
+
+$Id$
+
+UI declarations.
+
+*/
+
+#ifndef __FREEX3D_UI_H__
+#define __FREEX3D_UI_H__
+
+
+void openMainWindow (int argc, char **argv);
+
+
+#endif /* __FREEX3D_UI_H__ */
