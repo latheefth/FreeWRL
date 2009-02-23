888c1f72870fffdd793c99dc5f94cd04cde44138
istakenv <istakenv>
2009-02-23 22:01:33 +0000

    Fixed order of motif ldflags (was causing runtime errors)

--- freex3d/src/lib/system_js.h
+++ freex3d/src/lib/system_js.h
@@ -13,14 +13,18 @@ Internal header: Javascript engine dependencies.
 
 
 /* 
-   spidermonkey is built with the following flags:
+   spidermonkey is built with the following flags on Mac:
 
 -Wall -Wno-format -no-cpp-precomp -fno-common -DJS_THREADSAFE -DXP_UNIX -DSVR4 -DSYSV -D_BSD_SOURCE -DPOSIX_SOURCE -DDARWIN  -UDEBUG -DNDEBUG -UDEBUG_root -DJS_THREADSAFE -DEDITLINE
 
 */
 
-#include <jsapi.h> /* JS compiler */
-#include <jsdbgapi.h> /* JS debugger */
-
+#ifdef MOZILLA_JS_UNSTABLE_INCLUDES
+# include "../unstable/jsapi.h" /* JS compiler */
+# include "../unstable/jsdbgapi.h" /* JS debugger */
+#else
+# include <jsapi.h> /* JS compiler */
+# include <jsdbgapi.h> /* JS debugger */
+#endif
 
 #endif /* __LIBFREEWRL_SYSTEM_JS_H__ */
