f15f52640476099ad8afcd134927bfc37eb1e1c7
crc_canada <crc_canada>
2009-05-14 14:44:08 +0000

    Shader url, if an element contains "shader:" is a string, else a filename.

--- freex3d/src/lib/world_script/CScripts.h
+++ freex3d/src/lib/world_script/CScripts.h
@@ -96,8 +96,8 @@ void deleteScript();
 
 /* Initializes the script with its code */
 BOOL script_initCode(struct Shader_Script*, const char*);
-BOOL script_initCodeFromUri(struct Shader_Script*, const char*);
 BOOL script_initCodeFromMFUri(struct Shader_Script*, const struct Multi_String*);
+char **shader_initCodeFromMFUri(const struct Multi_String* s);
 
 /* Add a new field */
 void script_addField(struct Shader_Script*, struct ScriptFieldDecl*);
