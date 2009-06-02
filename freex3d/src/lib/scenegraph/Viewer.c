430a31fa42aeebb1d83273c653b9b6f6a2f2b89f
crc_canada <crc_canada>
2009-06-02 16:43:42 +0000

    Script routing debugs

--- freex3d/src/lib/scenegraph/Viewer.c
+++ freex3d/src/lib/scenegraph/Viewer.c
@@ -991,7 +991,7 @@ void bind_viewpoint (struct X3D_Viewpoint *vp) {
 	Viewer.examine->Origin.x = vp->centerOfRotation.c[0];
 	Viewer.examine->Origin.y = vp->centerOfRotation.c[1];
 	Viewer.examine->Origin.z = vp->centerOfRotation.c[2];
-printf ("BVP, origin %4.3f %4.3f %4.3f\n",Viewer.examine->Origin.x, Viewer.examine->Origin.y, Viewer.examine->Origin.z);
+	/* printf ("BVP, origin %4.3f %4.3f %4.3f\n",Viewer.examine->Origin.x, Viewer.examine->Origin.y, Viewer.examine->Origin.z); */
 
 	/* set Viewer position and orientation */
 
