5a88866c9b99aed52e503c36c7d4da2db5148821
crc_canada <crc_canada>
2009-06-12 20:46:46 +0000

    textures and TriangleSet nodes.

--- freex3d/src/lib/main/headers.h
+++ freex3d/src/lib/main/headers.h
@@ -632,7 +632,7 @@ extern int     *global_tcin;
 extern int     global_tcin_count; 
 extern void 	*global_tcin_lastParent;
 
-extern void textureDraw_start(struct X3D_IndexedFaceSet *texC, GLfloat *tex);
+extern void textureDraw_start(struct X3D_Node *texC, GLfloat *tex);
 extern void textureDraw_end(void);
 
 extern void * this_textureTransform;  /* do we have some kind of textureTransform? */
