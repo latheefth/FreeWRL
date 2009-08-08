51eabdf30c7ae74ff0d14fac43ddadc2c0be64bc
crc_canada <crc_canada>
2009-08-08 23:09:43 +0000

    new file

---
+++ freex3d/src/lib/scenegraph/Component_ProgrammableShaders.h
@@ -0,0 +1,15 @@
+/*
+=INSERT_TEMPLATE_HERE=
+
+$Id$
+
+Proximity sensor macro.
+
+*/
+
+#ifndef __FREEWRL_SCENEGRAPH_PROGRAMMABLESHADERS_H__
+#define __FREEWRL_SCENEGRAPH_PROGRAMMABLESHADERS_H__
+
+extern GLuint globalCurrentShader;
+
+#endif /* __FREEWRL_SCENEGRAPH_PROGRAMMABLESHADERS_H__ */
