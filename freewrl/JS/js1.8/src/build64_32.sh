1f2e736e9ab2e0acc44f4e376aceab9e8cff3180
crc_canada <crc_canada>
2010-02-12 19:34:53 +0000

    build the ppc in the middle so that Darwin_DBG.OBJ/jscpucfg can be run on machines without Rosetta installed.

--- freewrl/JS/js1.8/src/build64_32.sh
+++ freewrl/JS/js1.8/src/build64_32.sh
@@ -17,18 +17,6 @@ make -f Makefile.ref clean
 make -f Makefile.ref
 cp Darwin_DBG.OBJ/libjs.dylib Darwin_DBG.OBJ/libjs_i386.dylib
 
-cp Makefile.x86_64.ref Makefile.ref
-cp rules.x86_64.mk rules.mk
-cp config/Darwin.x86_64.mk config/Darwin.mk
-make -f Makefile.ref clean
-rm -f Darwin_DBG.OBJ/jscpucfg.o
-rm -f Darwin_DBG.OBJ/jscpucfg.h
-rm -f Darwin_DBG.OBJ/jscpucfg
-rm -f editline/Darwin_DBG.OBJ/libedit.a
-rm -f editline/Darwin_DBG.OBJ/*.o
-make -f Makefile.ref
-cp Darwin_DBG.OBJ/libjs.dylib Darwin_DBG.OBJ/libjs_x86_64.dylib
-
 cp Makefile.ppc.ref Makefile.ref
 cp rules.ppc.mk rules.mk
 cp config/Darwin.ppc.mk config/Darwin.mk
@@ -40,6 +28,18 @@ rm -f editline/Darwin_DBG.OBJ/libedit.a
 rm -f editline/Darwin_DBG.OBJ/*.o
 make -f Makefile.ref
 cp Darwin_DBG.OBJ/libjs.dylib Darwin_DBG.OBJ/libjs_ppc.dylib
+
+cp Makefile.x86_64.ref Makefile.ref
+cp rules.x86_64.mk rules.mk
+cp config/Darwin.x86_64.mk config/Darwin.mk
+make -f Makefile.ref clean
+rm -f Darwin_DBG.OBJ/jscpucfg.o
+rm -f Darwin_DBG.OBJ/jscpucfg.h
+rm -f Darwin_DBG.OBJ/jscpucfg
+rm -f editline/Darwin_DBG.OBJ/libedit.a
+rm -f editline/Darwin_DBG.OBJ/*.o
+make -f Makefile.ref
+cp Darwin_DBG.OBJ/libjs.dylib Darwin_DBG.OBJ/libjs_x86_64.dylib
 
 lipo -create Darwin_DBG.OBJ/libjs_i386.dylib Darwin_DBG.OBJ/libjs_x86_64.dylib Darwin_DBG.OBJ/libjs_ppc.dylib -output Darwin_DBG.OBJ/libjs.dylib
 rm -f Darwin_DBG.OBJ/libjs_i386.dylib
