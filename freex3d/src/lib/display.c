0c5213b60d35f49cac9a06d3033d52307273eceb
istakenv <istakenv>
2010-10-13 14:48:53 +0000

    quick debug for intel renderer string

--- freex3d/src/lib/display.c
+++ freex3d/src/lib/display.c
@@ -297,7 +297,7 @@ bool initialize_rdr_caps()
 	/* Special drivers settings */
 	printf("\n\n--%s--\n\n",rdr_caps.renderer);
 	if (
-	strnstr(rdr_caps.renderer, "Intel GMA 9", 11) != NULL &&
+	strnstr(rdr_caps.renderer, "Intel GMA 9", 11) != NULL ||
 	strstr(rdr_caps.renderer, "Intel(R) 9") != NULL
 	) {
 		if (rdr_caps.max_texture_size > 1024) rdr_caps.max_texture_size = 1024;
