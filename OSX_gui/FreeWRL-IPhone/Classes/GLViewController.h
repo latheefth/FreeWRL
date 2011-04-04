8e98ce6e656c567ee5cf8e4ef121fdf509396472
crc_canada <crc_canada>
2011-04-04 20:14:01 +0000

    putting some more touch stuff here; also can run with a file encoded in GLViewController.m file.

--- OSX_gui/FreeWRL-IPhone/Classes/GLViewController.h
+++ OSX_gui/FreeWRL-IPhone/Classes/GLViewController.h
@@ -9,16 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "GLView.h"
 
-/* get library definitions; do not get the rest of the
-   stuff in there, as there's lots of stuff that is hard
-   to compile here. */
-
-//#define COMPILING_IPHONE_FRONT_END
 #include "../../../freex3d/src/lib/libFreeWRL.h"
-//#undef COMPILING_IPHONE_FRONT_END
 
-
-@interface GLViewController : UIViewController <GLViewDelegate>
+@interface GLViewController : UIViewController <GLViewDelegate, UIGestureRecognizerDelegate>
 {
 
     //NSMutableDictionary *receivedData;
--- OSX_gui/FreeWRL-IPhone/Classes/GLViewController.m
+++ OSX_gui/FreeWRL-IPhone/Classes/GLViewController.m
@@ -20,17 +20,14 @@
     initializeRenderSceneUpdateScene();
     
     //OSX_initializeParameters("http://freewrl.sourceforge.net/test2pt.wrl");
-    OSX_initializeParameters(
-        //"http://freewrl.sourceforge.net/JAS/SSID-Mar2011/staticCount10.x3d");
-        //"http://dl.dropbox.com/u/17457/bike.wrl");
-    "http://freewrl.sourceforge.net/JAS/IFStests/bike-modified.wrl");
+   // OSX_initializeParameters( //"http://freewrl.sourceforge.net/JAS/SSID-Mar2011/staticCount500.x3d");
+                             //"http://freewrl.sourceforge.net/test2.wrl");
     
-                             //"http://freewrl.sourceforge.net/test.wrl");
-    
-    // simple cone OSX_initializeParameters("http://freewrl.sourceforge.net/test.wrl");
-
-    
+    //OSX_initializeParameters("http://kins.net/smar/files/ryatkins/aeroboat.wrl");
+	OSX_initializeParameters("http://freewrl.sourceforge.net/test3.wrl");
+    //OSX_initializeParameters("http://dl.dropbox.com/u/17457/aeroboat.wrl");
     //NSLog (@"ending loading thread");
+	
 }
 @end
 
@@ -39,6 +36,113 @@
 
 bool frontEndGettingFile = false;
 NSMutableData *receivedData;
+
+
+/* from the headers.h file - for now */
+#define KeyPress        2
+#define KeyRelease      3
+#define ButtonPress     4
+#define ButtonRelease   5
+#define MotionNotify    6
+#define MapNotify       19
+/* .... */
+
+
+#pragma mark -
+#pragma mark View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	
+   	UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapFrom:)];
+    [tapGesture setDelegate:self];
+	[self.view addGestureRecognizer:tapGesture];
+	[tapGesture release];
+        
+    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinchFrom:)];
+    [pinchGesture setDelegate:self];
+    [self.view addGestureRecognizer:pinchGesture];
+    [pinchGesture release];
+
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanFrom:)];
+    [panGesture setMaximumNumberOfTouches:1];
+    //[panGesture setDelegate:self];
+    [self.view addGestureRecognizer:panGesture];
+    [panGesture release];      
+    
+    UIRotationGestureRecognizer *rotationGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotationFrom:)];
+    [self.view addGestureRecognizer:rotationGesture];
+    [rotationGesture release];
+    
+}
+
+
+-(void)setupView:(GLView*)view
+{
+	//NSLog (@"setupView");
+	CGRect rect = view.bounds;
+    //NSLog (@"initialize - size %d %d",(int)rect.size.width, (int)rect.size.height);
+    
+	setScreenDim((int)rect.size.width, (int)rect.size.height);
+	display_initialize();
+	
+        // thread the getting of the file...
+    [NSThread detachNewThreadSelector:@selector(aMethod:) toTarget:[MyObject class] withObject:nil];
+    
+    
+	// NSLog (@"setupView complete");
+}
+
+
+- (void)drawView:(GLView*)view;
+{
+    //NSLog (@"drawing");
+    if (frontEndWantsFileName() != nil) {
+#define ccFUDGE_THIS_FOR_TESTING
+#ifdef FUDGE_THIS_FOR_TESTING
+        
+#define MYSTRING \
+"#VRML V2.0 utf8\n" \
+"# 10vertices.wrl\n" \
+        " Shape { appearance Appearance { material Material {} } geometry IndexedFaceSet { color Color {color [0 1 0]} colorPerVertex FALSE coord Coordinate { point [-1 0 0,0 0 0,1 0 0, 1 0.5 0,1 1 0,1 2 0, 0 2 0,-1 2 0,-1 1 0, -1 0.5 0]} coordIndex [0,1,2,3,4,5,6,7,8, 9,-1] solid FALSE } }"
+        
+        frontEndReturningData(MYSTRING, strlen(MYSTRING));
+        
+#else
+        if (!frontEndGettingFile) {
+            frontEndGettingFile = true;
+        
+        // Create the request. Get the file name from the FreeWRL library.
+        NSString *myString = [[NSString alloc] initWithUTF8String:frontEndWantsFileName()];
+        NSURLRequest *theRequest=
+            [NSURLRequest requestWithURL:[NSURL URLWithString: myString ]
+                             cachePolicy:NSURLRequestUseProtocolCachePolicy
+                         timeoutInterval:60.0];
+
+            
+            // create the connection with the request
+        // and start loading the data
+        NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
+
+        if (theConnection) {
+            // Create the NSMutableData to hold the received data.
+            // receivedData is an instance variable declared elsewhere.
+            receivedData = [[NSMutableData data] retain];
+         
+        } else {
+            // Inform the user that the connection failed.
+        }
+        }
+#endif
+    }
+
+    RenderSceneUpdateScene();
+	
+}
+
+#pragma mark -
+#pragma mark Connections
+
 
 -(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
@@ -55,7 +159,7 @@ NSMutableData *receivedData;
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
-    //NSLog (@"connection: didReceiveData called");
+    NSLog (@"connection: didReceiveData called");
     // Append the new data to receivedData.
     // receivedData is an instance variable declared elsewhere.
     [receivedData appendData:data];
@@ -96,8 +200,7 @@ NSMutableData *receivedData;
 
     free (myData);
     frontEndGettingFile = false;
-
-
+    
     // release the connection, and the data object
     [connection release];
     [receivedData release];
@@ -105,167 +208,134 @@ NSMutableData *receivedData;
 
 
 
+#pragma mark -
+#pragma mark Responding to gestures
 
--(void)setupView:(GLView*)view
-{
-	//NSLog (@"setupView");
-	CGRect rect = view.bounds;
-      //NSLog (@"initialize - size %d %d",(int)rect.size.width, (int)rect.size.height);
-    
-	setScreenDim((int)rect.size.width, (int)rect.size.height);
-	display_initialize();
 
-    // thread the getting of the file...
-    [NSThread detachNewThreadSelector:@selector(aMethod:) toTarget:[MyObject class] withObject:nil];
-    
-    
-	// NSLog (@"setupView complete");
+/*
+ In response to a tap gesture
+*/
+- (void)handleTapFrom:(UITapGestureRecognizer *)recognizer {
+	
+	CGPoint loc = [recognizer locationInView:self.view];
+	NSLog(@"TAP x:%f y:%f", loc.x, loc.y);
 }
 
-- (void)drawView:(GLView*)view;
-{
-    //NSLog (@"drawing");
-    if (frontEndWantsFileName() != nil) {
-#define ccFUDGE_THIS_FOR_TESTING
-#ifdef FUDGE_THIS_FOR_TESTING
-#define MYSTRING "#VRML V2.0 utf8\n" \
-        "Shape {geometry Sphere {}}"
-        
-#define MYSTRING \
-"#VRML V2.0 utf8\n" \
-"# 10vertices.wrl\n" \
-        " Shape { appearance Appearance { material Material {} } geometry IndexedFaceSet { color Color {color [0 1 0]} colorPerVertex FALSE coord Coordinate { point [-1 0 0,0 0 0,1 0 0, 1 0.5 0,1 1 0,1 2 0, 0 2 0,-1 2 0,-1 1 0, -1 0.5 0]} coordIndex [0,1,2,3,4,5,6,7,8, 9,-1] solid FALSE } }"
-        
-#define XMYSTRING \
-"#VRML V2.0 utf8\n" \
-"# 10vertices.wrl - no color \n" \
-" Shape { appearance Appearance { material Material {} } geometry IndexedFaceSet {  coord Coordinate { point [-1 0 0,0 0 0,1 0 0, 1 0.5 0,1 1 0,1 2 0, 0 2 0,-1 2 0,-1 1 0, -1 0.5 0]} coordIndex [0,1,2,3,4,5,6,7,8, 9,-1] solid FALSE } }"
 
+/*
+ In response to a pinch gesture
+*/
+- (void)handlePinchFrom:(UIPinchGestureRecognizer *)recognizer {
+	
+    // location is only recorded once
+	CGPoint loc = [recognizer locationInView:self.view];
+    CGFloat scale = recognizer.scale;	    
+    // crude calculation to get the distance by using the screen height and the scale
+    CGFloat height = [[UIScreen mainScreen] bounds].size.height;
+    CGFloat distance = (height / 5) * scale; 
+    
+    // FIRST TOUCH
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+        setButDown(3, 1);
+        setLastMouseEvent(ButtonPress);        
+        handle_aqua(ButtonPress,3,(int)loc.x,(int)distance);
+        NSLog(@"==== pinch first touch ====");
         
-#define XMYSTRING \
-"#VRML V2.0 utf8\n" \
-"# 10vertices.wrl - no color \n" \
-" Shape { appearance Appearance { material Material {} } geometry IndexedFaceSet { coord Coordinate{ " \
-"point [  -3.0 1.5 1.0, 3.0 1.5 1.0, 3.0 1.5 -1.0, -3.0 1.5 -1.0, -3.0 -1.5 1.0, 3.0 -1.5 1.0, 3.0 -1.5 -1.0, -3.0 -1.5 -1.0,  ] } " \
-" coordIndex [ 0, 1, 2, 3, -1, 7, 6, 5, 4, -1, 0, 4, 5, 1, -1,  1, 5, 6, 2, -1, 2, 6, 7, 3, -1, 3, 7, 4, 0,  ] } } "
-
+    // END    
+    } else if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled) {        
         
+        setButDown(3, 0);
+        setLastMouseEvent(ButtonRelease);
+        handle_aqua(ButtonRelease,3,(int)loc.x,(int)loc.y);       
         
-
-        
-        frontEndReturningData(MYSTRING, strlen(MYSTRING));
-        
-#else
-        if (!frontEndGettingFile) {
-            frontEndGettingFile = true;
-        
-        // Create the request. Get the file name from the FreeWRL library.
-        NSString *myString = [[NSString alloc] initWithUTF8String:frontEndWantsFileName()];
-        NSURLRequest *theRequest=
-            [NSURLRequest requestWithURL:[NSURL URLWithString: myString ]
-                             cachePolicy:NSURLRequestUseProtocolCachePolicy
-                         timeoutInterval:60.0];
-
-            
-            // create the connection with the request
-        // and start loading the data
-        NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:theRequest delegate:self];
-
-        if (theConnection) {
-            // Create the NSMutableData to hold the received data.
-            // receivedData is an instance variable declared elsewhere.
-            receivedData = [[NSMutableData data] retain];
-         
-        } else {
-            // Inform the user that the connection failed.
-        }
-        }
-#endif
+    // CONTINUING
+    } else if (recognizer.state == UIGestureRecognizerStateChanged) {        
+        setButDown(3, 1);
+        setLastMouseEvent(MotionNotify);       
+                
+        handle_aqua(MotionNotify,3,(int)loc.x,(int)distance);
+        NSLog(@"SCALE y:%f h:%f scale:%f", loc.y, height, scale);
     }
+}
 
-    RenderSceneUpdateScene();
+/*
+ In response to a pan gesture
+*/
+- (void)handlePanFrom:(UIPanGestureRecognizer *)recognizer {
+    
+	CGPoint loc = [recognizer locationInView:self.view];
+    
+    // Velocity could be used here
+    //CGPoint velocity = [recognizer velocityInView:self.view];
+
+    
+    // FIRST TOUCH
+    if (recognizer.state == UIGestureRecognizerStateBegan) {
+        
+        setButDown(1, 1);
+        setLastMouseEvent(ButtonPress);    
+        //setCurXY((int)loc.x, (int)loc.y);
+        handle_aqua(ButtonPress,1,(int)loc.x,(int)loc.y);      
+        
+    // END    
+    } else if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled) {        
+        
+        setButDown(1, 0);
+        setLastMouseEvent(ButtonRelease);
+        //setCurXY((int)loc.x, (int)loc.y);
+        handle_aqua(ButtonRelease,1,(int)loc.x,(int)loc.y);       
+        
+    // CONTINUING
+    } else if (recognizer.state == UIGestureRecognizerStateChanged) {        
+        setButDown(1, 1);
+        setLastMouseEvent(MotionNotify);
+        
+        //setCurXY((int)loc.x, (int)loc.y);
+        handle_aqua(MotionNotify,1,(int)loc.x,(int)loc.y);
+        NSLog(@"PAN x:%f y:%f", loc.x, loc.y);
+    }	
+   
+}
+
+
+/*
+ In response to a rotation gesture
+*/
+- (void)handleRotationFrom:(UIRotationGestureRecognizer *)recognizer {
+	
+	CGPoint loc = [recognizer locationInView:self.view];
+    
+    CGAffineTransform transform = CGAffineTransformMakeRotation([recognizer rotation]);
+    
+}
+
+/*
+ In response to a swipe gesture.
+ */
+- (void)handleSwipeFrom:(UISwipeGestureRecognizer *)recognizer {
+    
+        
+    
+	CGPoint loc = [recognizer locationInView:self.view];
+	
+	
+    if (recognizer.direction == UISwipeGestureRecognizerDirectionLeft) {
+        
+    }
+    else {
+        
+    }
 	
 }
+
+#pragma mark -
+#pragma mark Memory management
 
 - (void)dealloc {
 	// JASfinalizeRenderSceneUpdateScene();
     [super dealloc];
 }
 
-
-#pragma mark -
-#pragma mark touch events
-
-/* from the headers.h file - for now */
-#define KeyPress        2
-#define KeyRelease      3
-#define ButtonPress     4
-#define ButtonRelease   5
-#define MotionNotify    6
-#define MapNotify       19
-/* .... */
-
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
-{    
-           
-    setButDown(1, 1); 
-    setLastMouseEvent(ButtonPress);
-    for (UITouch *t in touches) 
-    {
-        CGPoint loc = [t locationInView:self.view];
-        //NSLog(@"x:%f y:%f", loc.x, loc.y);
-        
-        setCurXY((int)loc.x, (int)loc.y);
-        handle_aqua(ButtonPress,1,(int)loc.x,(int)loc.y);
-    }
-
-}
-
-- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    setButDown(1, 1);
-    setLastMouseEvent(MotionNotify);
-    
-    for (UITouch *t in touches) 
-    {
-        CGPoint loc = [t locationInView:self.view];
-        //NSLog(@"x:%f y:%f", loc.x, loc.y);
-        
-        setCurXY((int)loc.x, (int)loc.y);
-        handle_aqua(MotionNotify,1,(int)loc.x,(int)loc.y);
-    }
-    
-}
-
-- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    setButDown(1, 0);
-    setLastMouseEvent(ButtonRelease);
-    for (UITouch *t in touches) 
-    {
-        CGPoint loc = [t locationInView:self.view];
-        //NSLog(@"x:%f y:%f", loc.x, loc.y);
-        
-        setCurXY((int)loc.x, (int)loc.y);
-        handle_aqua(ButtonRelease,1,(int)loc.x,(int)loc.y);
-    }
-    
-}
-
-- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
-{
-    setButDown(1, 0);
-    setLastMouseEvent(ButtonRelease);
-    for (UITouch *t in touches) 
-    {
-        CGPoint loc = [t locationInView:self.view];
-        //NSLog(@"x:%f y:%f", loc.x, loc.y);
-        
-        setCurXY((int)loc.x, (int)loc.y);
-        handle_aqua(ButtonRelease,1,(int)loc.x,(int)loc.y);
-    }
-}
 
 @end
 
