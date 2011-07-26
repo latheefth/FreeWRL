208ab244e3dceb6b9712d7796ddd2e9b19261c70
dug9 <dug9>
2011-07-26 14:09:07 +0000

    dug9 - reload > kill_oldWorld > globalParser = NULL wrong, needs to zero in iglobal

--- freex3d/src/lib/opengl/OpenGL_Utils.c
+++ freex3d/src/lib/opengl/OpenGL_Utils.c
@@ -2505,7 +2505,7 @@ void kill_oldWorld(int kill_EAI, int kill_JavaScript, char *file, int line) {
         char mystring[20];
 	#endif
 	struct VRMLParser *globalParser = (struct VRMLParser *)gglobal()->CParse.globalParser;
-
+	printf("start kill_oldWorld\n");
 #ifdef VERBOSE
 	printf ("kill 1 myThread %u displayThread %u\n",pthread_self(), gglobal()->threads.DispThrd);
 #ifdef _MSC_VER
@@ -2587,13 +2587,15 @@ void kill_oldWorld(int kill_EAI, int kill_JavaScript, char *file, int line) {
 	/* reset any VRML Parser data */
 	if (globalParser != NULL) {
 		parser_destroyData(globalParser);
-		globalParser = NULL;
+		//globalParser = NULL;
+		gglobal()->CParse.globalParser = NULL;
 	}
 
 
 	/* tell statusbar that we have none */
 	viewer_default();
 	setMenuStatus("NONE");
+	printf("end kill_oldWorld\n");
 }
 
 /* for verifying that a memory pointer exists */
