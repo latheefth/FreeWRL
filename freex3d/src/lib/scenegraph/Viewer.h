0026d2c527a4bdb8cb9cc660f245d4058b69d301
dug9 <dug9>
2012-05-01 14:35:38 +0000

    dug9 - more nav types (fly2,tilt,tplane,rplane) for non-keyboard mobile devices

--- freex3d/src/lib/scenegraph/Viewer.h
+++ freex3d/src/lib/scenegraph/Viewer.h
@@ -184,6 +184,8 @@ typedef struct viewer_ypz {
 
 typedef struct viewer_inplane {
 	float x,y;
+	float xx,yy;
+	int on;
 } X3D_Viewer_InPlane;
 
 typedef struct key {
@@ -234,7 +236,7 @@ typedef struct viewer {
 	int iprog[2]; /*which shader program for left vp,right vp 0-5 */
 	/* */
 	unsigned int buffer;
-	int oktypes[7];		/* boolean for types being acceptable. */
+	int oktypes[16];		/* boolean for types being acceptable. */
 	X3D_Viewer_Walk walk;
 	X3D_Viewer_Examine examine;
 	X3D_Viewer_Fly fly;
