af04058c23563d9525090b739762a80ecc4d6e78
crc_canada <crc_canada>
2012-06-01 15:00:07 +0000

    June 1 Desktop build - inlcudes libtess and libfreetype.

--- OSX_gui/FreeWRL-Desktop/FreeWRL/config.h
+++ OSX_gui/FreeWRL-Desktop/FreeWRL/config.h
@@ -3,7 +3,7 @@
 #define SHADERS_2011 1
 
 #define INSTALLDIR "/usr/share/freewrl"
-#define FONTS_DIR "/usr/share/freewrl/fonts"
+#define FONTS_DIR "/Applications/FreeWRL/fonts"
 #define X_DISPLAY_MISSING 1
 #define HAVE_STDARG_H 1
 #define HAVE_ERRNO_H 1
@@ -21,6 +21,8 @@
 #define HAVE_GETOPT_H 1
 #define FRONTEND_HANDLES_DISPLAY_THREAD 1
 #define FRONTEND_GETS_FILES 1
+#define HAVE_GLU_TESS 1
+
 #define BUILDDIR "/Users/john/Desktop/Android-tests-current/freewrl/freex3d/appleOSX"
 #define FWVER "1.22.13"
 #define FREEX3D_MESSAGE_WRAPPER "/usr/bin/say"
