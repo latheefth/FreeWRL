9aa4f1d6343a2856c0e2ec730bbd281e6e425f9e
istakenv <istakenv>
2012-06-06 15:26:06 +0000

    updated latest version with debug message change from freewrl-1.22.13

--- freex3d/src/lib/opengl/Frustum.c
+++ freex3d/src/lib/opengl/Frustum.c
@@ -512,8 +512,10 @@ void setExtent(float maxx, float minx, float maxy, float miny, float maxz, float
 	me->EXTENT_MAX_Z = maxz; me->EXTENT_MIN_Z = minz;
 
 	if (me->_parentVector == NULL) {
+		#ifdef FRUSTUMVERBOSE
 		printf ("setExtent, parentVector NULL for node %p type %s\n",
 			me,stringNodeType(me->_nodeType));
+		#endif
 		return;
 	}
 
