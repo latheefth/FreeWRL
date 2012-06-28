809b5eee13acf73b1c5f75c28fba7e6aaed91357
crc_canada <crc_canada>
2012-06-28 15:10:15 +0000

    change the contact email

--- Android/src/org/freewrl/FreeWRLVersion.java
+++ Android/src/org/freewrl/FreeWRLVersion.java
@@ -26,5 +26,5 @@ package org.freewrl;
 
 public class FreeWRLVersion {
 	public static String version = "1.22.13";
-	public static String compileDate = "2012-06-26:16:59:25";
+	public static String compileDate = "2012-06-28:08:33:50";
 }
