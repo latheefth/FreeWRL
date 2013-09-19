f849050eb1780624d93a8b926310f279cd8b9ea9
dug9 <dug9>
2013-09-19 21:11:31 +0000

    dug9 - touch up for buildversion.h

--- freex3d/projectfiles_vc9/fwVersion.c
+++ freex3d/projectfiles_vc9/fwVersion.c
@@ -1,2 +1,4 @@
-char *freewrl_get_version(void) { return "2.0";}char *libFreeWRL_get_version(void) {return "2.0";}
+//#include <libversion.h>
+//const char *libFreeWRL_get_version(void) {return FW_LIB_VERSION_STR;}
+const char *freewrl_get_version(void) { return libFreeWRL_get_version();}
 
