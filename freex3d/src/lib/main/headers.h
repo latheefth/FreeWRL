c855eb4288c1ed9840e7c6b120c1d96c73ba8f8a
crc_canada <crc_canada>
2013-10-23 15:56:38 +0000

    *** empty log message ***

--- freex3d/src/lib/main/headers.h
+++ freex3d/src/lib/main/headers.h
@@ -199,31 +199,21 @@ node for ANY node that takes something other than a Group */
 
 #define offsetPointer_deref(t, me, offs) ((t)(((char*)(me))+offs))
 
-//before Brotos/Proto
-//#define POSSIBLE_PROTO_EXPANSION(type,inNode,outNode) \
-//	if (inNode == NULL) outNode = NULL; \
-//	else {if (X3D_NODE(inNode)->_nodeType == NODE_Group) { \
-//		if (X3D_GROUP(inNode)->children.n>0) { \
-//			outNode = (type)(X3D_GROUP(inNode)->children.p[0]); \
-//		} else outNode = NULL; \
-//	} else outNode = (type)inNode; };
+/* before Brotos/Proto
+#define POSSIBLE_PROTO_EXPANSION(type,inNode,outNode) \
+	if (inNode == NULL) outNode = NULL; \
+	else {if (X3D_NODE(inNode)->_nodeType == NODE_Group) { \
+		if (X3D_GROUP(inNode)->children.n>0) { \
+			outNode = (type)(X3D_GROUP(inNode)->children.p[0]); \
+		} else outNode = NULL; \
+	} else outNode = (type)inNode; };
+*/
+
 struct X3D_Node* getTypeNode(struct X3D_Node *node);
 
 //with Proto for Brotos
 #define POSSIBLE_PROTO_EXPANSION(type,inNode,outNode) \
 	outNode = (type)getTypeNode(inNode);
-	//if (inNode == NULL) outNode = NULL; \
-	//else { \
-	//  if (X3D_NODE(inNode)->_nodeType == NODE_Group) { \
-	//	if (X3D_GROUP(inNode)->children.n>0) { \
-	//		outNode = (type)(X3D_GROUP(inNode)->children.p[0]); \
-	//	} else outNode = NULL; \
-	//  } else if (X3D_NODE(inNode)->_nodeType == NODE_Proto) { \
-	//		outNode = (type)getTypeNode(inNode); \
-	//  } else outNode = (type)inNode; \
- //   }
-
-
 
 #define MARK_NODE_COMPILED node->_ichange = node->_change;
 #define NODE_NEEDS_COMPILING (node->_ichange != node->_change)
