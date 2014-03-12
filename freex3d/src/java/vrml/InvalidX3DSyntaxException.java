c4cdafa81a71eb8e7dcc5db0761afdffe23ee1a8
Ian Stakenvicius <axs@gentoo.org>
2014-03-12 12:17:04 -0400

    Created missing InvalidX3DSyntaxException class to fix java builds
    It seems that this file may be been created locally as part of
    commit 8f0445c0 but accidentally wasn't added to the repo
    
    Signed-off-by: Ian Stakenvicius <axs@gentoo.org>

---
+++ freex3d/src/java/vrml/InvalidX3DSyntaxException.java
@@ -0,0 +1,6 @@
+package vrml;
+
+public class InvalidX3DSyntaxException extends Exception {
+    public InvalidX3DSyntaxException() { super(); }
+    public InvalidX3DSyntaxException(String s) { super(s); }
+}
