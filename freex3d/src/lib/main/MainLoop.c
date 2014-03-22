5f3922d4731b31c8f48941c80b5bc8fb1c11c77c
dug9 <highaspirations@hotmail.com>
2014-03-22 10:50:08 -0600

    win32 sys clock function change
    GetTickCount64 works with 47.wrl, 49.wrl interpolations, GetSystemTimeAsFileTime > Quadpart doesnt.

--- freex3d/src/lib/main/MainLoop.c
+++ freex3d/src/lib/main/MainLoop.c
@@ -513,14 +513,14 @@ double Time1970sec()
 
   // GetLocalTime(&mytimet);
   // return (double) mytimet.wHour*3600.0 + (double)mytimet.wMinute*60.0 + (double)mytimet.wSecond + (double)mytimet.wMilliseconds/1000.0;
-	FILETIME ft;
-	ULARGE_INTEGER ul;
-	GetSystemTimeAsFileTime(&ft); //higher resolution 100nanosec but systemtime can change with internet updates
-	ul.LowPart = ft.dwLowDateTime;
-	ul.HighPart = ft.dwHighDateTime;
-	return ((double)(ul.QuadPart))*.0000001; //they say 100 nanosecond resolution, but .1 nanoseconds seems to work
+	//FILETIME ft;
+	//ULARGE_INTEGER ul;
+	//GetSystemTimeAsFileTime(&ft); //higher resolution 100nanosec but systemtime can change with internet updates
+	//ul.LowPart = ft.dwLowDateTime;
+	//ul.HighPart = ft.dwHighDateTime;
+	//return ((double)(ul.QuadPart))*.0000001; //they say 100 nanosecond resolution, but .1 nanoseconds seems to work
 		
-	//return ((double)(GetTickCount64())) * .001; //in millisec with 10 to 16ms resolution, lower resolution but no internet updates
+	return ((double)(GetTickCount64())) * .001; //in millisec with 10 to 16ms resolution, lower resolution but no internet updates
 }
 
 /* #endif */
