ec7dc2acb6a0319508b8321eea917fdd8f8d61fc
dug9 <highaspirations@hotmail.com>
2014-06-18 06:29:38 -0600

    adjusted stubs
    - builds but bombs on JSparamnames

--- freex3d/src/lib/world_script_stub/JScript.c
+++ freex3d/src/lib/world_script_stub/JScript.c
@@ -6,16 +6,16 @@ typedef union anyVrml{
 void JScript_init(void *t){}
 
 /* stubs, when you don't have a javascript engine */
-void kill_javascript(void){return;}
-void JSInit(int num){return;}
-void SaveScriptText(int num, const char *text){return;}
+//void kill_javascript(void){return;}
+//void JSInit(int num){return;}
+//void SaveScriptText(int num, const char *text){return;}
 void process_eventsProcessed(){return;}
 void js_cleanup_script_context(int counter){return;}
 int jsActualrunScript(int num, char *script){return 0;}
-void JSInitializeScriptAndFields (int num){return;}
+//void JSInitializeScriptAndFields (int num){return;}
 void JSCreateScriptContext(int num){return;}
 
-void SaveScriptField (int num, indexT kind, indexT type, const char* field, union anyVrml value){return;}
+//void SaveScriptField (int num, indexT kind, indexT type, const char* field, union anyVrml value){return;}
 void js_setField_javascriptEventOut_B(union anyVrml* any, int fieldType, unsigned len, int extraData, int actualscript){return;}
 void js_setField_javascriptEventOut(struct X3D_Node *tn,unsigned int tptr,  int fieldType, unsigned len, int extraData, int actualscript){return;}
 
@@ -27,3 +27,10 @@ int JSparamIndex (const char *name, const char *type){return 0;}
 void set_one_ECMAtype (int tonode, int toname, int dataType, void *Data, int datalen){return;}
 void set_one_MultiElementType (int tonode, int tnfield, void *Data, int dataLen){return;}
 void set_one_MFElementType(int tonode, int toname, int dataType, void *Data, int datalen){return;}
+int jsIsRunning(){return 1;}
+void JSDeleteScriptContext(int num){return;}
+void jsShutdown(){return;}
+void InitScriptField(int num, indexT kind, indexT type, const char* field, union anyVrml value)
+{return;}
+void jsClearScriptControlEntries(struct CRscriptStruct *ScriptControl){return;}
+
