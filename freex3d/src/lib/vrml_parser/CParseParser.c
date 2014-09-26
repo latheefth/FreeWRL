1ab456570d6f27e763b73eddeb3640f565114b63
dug9 <highaspirations@hotmail.com>
2014-09-26 09:56:41 -0600

    turn off useBrotos = for merging

--- freex3d/src/lib/vrml_parser/CParseParser.c
+++ freex3d/src/lib/vrml_parser/CParseParser.c
@@ -80,7 +80,7 @@ void CParseParser_init(struct tCParseParser *t){
 	{
 		ppCParseParser p = (ppCParseParser)t->prv;
 		p->foundInputErrors = 0;
-		p->useBrotos = 3; //0= none/old-way, non-zero =wrl parsing broto only rendering, DEF/IS/script tables are in new proto 3=EXTERNPROTO is broto wrapper
+		p->useBrotos = 0; //0= none/old-way, non-zero =wrl parsing broto only rendering, DEF/IS/script tables are in new proto 3=EXTERNPROTO is broto wrapper
 	}
 }
 	//ppCParseParser p = (ppCParseParser)gglobal()->CParseParser.prv;
