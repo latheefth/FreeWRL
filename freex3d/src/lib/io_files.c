ef501c6f5d50f045a61e7048bfd876300a0835f2
dug9 <highaspirations@hotmail.com>
2015-01-20 12:53:39 -0700

    linux > Imlib2 image load function now works
    - imlib2 likes to stay on the same thread
    - changing to task_enqueue funnels/gathers threadpool thread results and enqueues for resource thread to call the image loader
    - win32 and linux desktop checked for local and http scene 47.wrl

--- freex3d/src/lib/io_files.c
+++ freex3d/src/lib/io_files.c
@@ -1255,7 +1255,7 @@ void loadAsync (resource_item_t *res) {
 void file2blob_task(s_list_t *item){
 	//chain, spawn async/thread, or re-enqueue FILE2BLOB to some work thread
 	resource_item_t *res = item->elem;
-	int tactic = file2blob_task_chain; //file2blob_task_spawn;
+	int tactic = file2blob_task_enqueue; // linux>imlib2 likes to stay on same thread //file2blob_task_chain; //file2blob_task_spawn;
 	if(tactic == file2blob_task_chain){
 		//chain FILE2BLOB
 		if(res->media_type == resm_image){
