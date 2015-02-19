2ac39dd23e9553ae6520f1089d0ec0f9e1d294c6
dug9 <highaspirations@hotmail.com>
2015-02-19 11:07:23 -0700

    Added flat-C interface to dllfreewrl
    - not tested yet

--- freex3d/src/dllFreeWRL/cdllFreeWRL.c
+++ freex3d/src/dllFreeWRL/cdllFreeWRL.c
@@ -8,57 +8,19 @@
   Here we'll use a pointer to iglobal as a context handle.
 
 */ 
-
-
 #include "stdafx.h"
-
-#include "dllFreeWRL.h"
-
+#include "cdllFreeWRL.h"
 #include <windows.h>
 #if _MSC_VER > 1700
 #include "winapifamily.h"
 #endif
-
 #include <stdio.h>
 #include <stdarg.h>
 #include <wtypes.h>
-//#define CONSOLE _DEBUG
+#include <sys/types.h>
+#include "../../src_windows/config.h"
+#include "system.h"
 
-#ifdef CONSOLE
-#undef CONSOLE
-static HANDLE hStdErr = NULL;
-static void swInitConsole(void)
-{
-    BOOL ac = AllocConsole();
-    if (ac)
-        hStdErr = GetStdHandle(STD_ERROR_HANDLE);
-}
-void swDebugf(LPCSTR formatstring, ...)
-{
-    char buff[2500];
-    DWORD cWritten;
-	int nSize;
-   memset(buff, 0, sizeof(buff));
-   va_list args;
-   va_start(args, formatstring);
-   
-   nSize = _vsnprintf( buff, sizeof(buff) - 1, formatstring, args); // C4996
-    if (hStdErr == NULL)
-        swInitConsole(); 
-    //_vsnprintf(buff, BUFSIZE, fmt, ap);
-    //OutputDebugStringA(buff);
-
-    /* not C console - more low level windows SDK API */
-    WriteConsoleA(hStdErr, buff, strlen(buff),&cWritten, NULL);
-}
-#else
-//int ConsoleMesage(char *_Format,...);
-//#define swDebugf ConsoleMessage
-int swDebugf(LPCSTR formatstring, ...) {return 0;}
-#endif
-
-extern "C"
-{
 #include "libFreeWRL.h"
 #include "ui/statusbar.h"
 void initConsoleH(DWORD pid);
@@ -75,7 +37,7 @@ void fwl_resitem_enqueuNextMulti(void *res);
 void fwl_resitem_setLocalPath(void *res, char* path);
 void fwl_resitem_enqueue(void *res);
 int file2blob(void *res);
-}
+
 
 #include <malloc.h>
 #include <WinUser.h>
@@ -84,7 +46,7 @@ int file2blob(void *res);
 
 // This is the constructor of a class that has been exported.
 // see dllFreeWRL.h for the class definition
-CdllFreeWRL::CdllFreeWRL()
+DLLFREEWRL_API void * dllFreeWRL_dllFreeWRL()
 {
 	/*STA -single threaded app- frontends -like web pages in a browser, .net forms, xaml apps-
 	can have multiple freewrl instances in different sub-windows, all running 
@@ -103,18 +65,18 @@ CdllFreeWRL::CdllFreeWRL()
 	the fwglobal pointer between calls.
 	*/
 	//this->globalcontexthandle = 0;
-	this->globalcontexthandle = fwl_init_instance(); //before setting any structs we need a struct allocated
+	return fwl_init_instance(); //before setting any structs we need a struct allocated
 }
 //	handle - window handle or null
 //		- if you have a window already created, you should pass in the handle, 
 //		- else pass null and a window will be created for you
-void CdllFreeWRL::onInit(int width, int height, void* windowhandle, bool bEai, bool frontend_handles_display_thread)
+DLLFREEWRL_API void dllFreeWRL_onInit(void *fwctx, int width, int height, void* windowhandle, int bEai, int frontend_handles_display_thread)
 {
 	int ok;
 	struct freewrl_params *params;
 	//if( !fwl_setCurrentHandle(handle) ){
 	//this->globalcontexthandle = fwl_init_instance(); //before setting any structs we need a struct allocated
-	fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__);
+	fwl_setCurrentHandle(fwctx, __FILE__, __LINE__);
 	/* Before we parse the command line, setup the FreeWRL default parameters */
 	params = (freewrl_params_t*) malloc( sizeof(freewrl_params_t));
 	memset(params,0,sizeof(freewrl_params_t));
@@ -125,12 +87,7 @@ void CdllFreeWRL::onInit(int width, int height, void* windowhandle, bool bEai, b
 	params->fullscreen = 0;
 	params->winToEmbedInto = (long)windowhandle;
 	params->frontend_handles_display_thread = frontend_handles_display_thread;
-	swDebugf("just before fwl_initFreeWRL\n");
 	ok = fwl_initFreeWRL(params);
-	//if(!ok){
-		//ERROR_MSG("main: aborting during initialization.\n");
-		//exit(1);
-	//} 
 #ifndef FRONTEND_HANDLES_DISPLAY_THREAD
 	if(ok)
 		if(!frontend_handles_display_thread)
@@ -141,103 +98,78 @@ void CdllFreeWRL::onInit(int width, int height, void* windowhandle, bool bEai, b
 #else
 	fwl_setScreenDim(width, height);
 #endif
-	//printf("press key to continue..:");
-	//getchar();
 	fwl_clearCurrentHandle();
 	return;
 }
-void CdllFreeWRL::setTempFolder(char *tmpFolder)
+DLLFREEWRL_API void dllFreeWRL_setTempFolder(void *fwctx, char *tmpFolder)
 {
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		fwl_tmpFileLocation(tmpFolder);
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::setFontFolder(char *fontFolder)
+DLLFREEWRL_API void dllFreeWRL_setFontFolder(void *fwctx,char *fontFolder)
 {
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		fwl_fontFileLocation(fontFolder);
 	}
 	fwl_clearCurrentHandle();
 }
-CdllFreeWRL::CdllFreeWRL(int width, int height, void* windowhandle, bool bEai)
+DLLFREEWRL_API void * dllFreeWRL_dllFreeWRL1(int width, int height, void* windowhandle, int bEai)
 {
-	this->globalcontexthandle = fwl_init_instance(); //before setting any structs we need a struct allocated
-	this->onInit(width, height, windowhandle, bEai);
+	void *fwctx;
+	fwctx = fwl_init_instance(); //before setting any structs we need a struct allocated
+	dllFreeWRL_onInit(fwctx, width, height, windowhandle, bEai, FALSE);
+	return fwctx;
 }
-CdllFreeWRL::CdllFreeWRL(char* scene_url, int width, int height, void* windowhandle, bool bEai)
+DLLFREEWRL_API void *dllFreeWRL_dllFreeWRL2(char* scene_url, int width, int height, void* windowhandle, int bEai)
 {
-	this->globalcontexthandle = fwl_init_instance(); //before setting any structs we need a struct allocated
-	this->onInit(width, height, windowhandle, bEai);
-	this->onLoad(scene_url);
+	void *fwctx;
+	fwctx = fwl_init_instance(); //before setting any structs we need a struct allocated
+	dllFreeWRL_onInit(fwctx, width, height, windowhandle, bEai, FALSE);
+	dllFreeWRL_onLoad(fwctx,scene_url);
+	return fwctx;
 }
 
-	//enum class KeyAction {KEYDOWN,KEYUP,KEYPRESS};
-	//enum class MouseAction {MOUSEMOVE,MOUSEDOWN,MOUSEUP};
-	//enum class MouseButton {LEFT,MIDDLE,RIGHT,NONE};
-//extern "C"{
-//int fv_display_initialize(void);
-//}
-
-void CdllFreeWRL::onLoad(char* scene_url)
+DLLFREEWRL_API void dllFreeWRL_onLoad(void *fwctx, char* scene_url)
 {
-	//char * url;
+	char * url;
 	url = _strdup(scene_url);
-	if(fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
-		//url = strBackslash2fore(url);
-		//swDebugf("onLoad have url=[%s]\n",url);
+	if(fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		fwl_replaceWorldNeeded(url);
-		//swDebugf("onLoad after push_single_request url=[%s]\n",url);
 	}
 	fwl_clearCurrentHandle();
-
 }
 
 
-void CdllFreeWRL::onResize(int width,int height){
-	if(fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
-		//swDebugf("onResize before\n");
+DLLFREEWRL_API void dllFreeWRL_onResize(void *fwctx, int width,int height){
+	if(fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 #ifdef STATUSBAR_HUD
 		statusbar_set_window_size(width,height);
 #else
 		fwl_setScreenDim(width,height);
 #endif
-		//swDebugf("onResize after\n");
 	}
 	fwl_clearCurrentHandle();
 }
-//#define KeyChar         1
-//#if defined(AQUA) || defined(WIN32)
-//#define KeyPress        2
-//#define KeyRelease      3
-//#define ButtonPress     4
-//#define ButtonRelease   5
-//#define MotionNotify    6
-//#define MapNotify       19
-//#endif
 
-void CdllFreeWRL::onMouse(int mouseAction,int mouseButton,int x, int y){
+DLLFREEWRL_API void dllFreeWRL_onMouse(void *fwctx, int mouseAction,int mouseButton,int x, int y){
 
 	/*void fwl_handle_aqua(const int mev, const unsigned int button, int x, int y);*/
 	/* butnum=1 left butnum=3 right (butnum=2 middle, not used by freewrl) */
-	//fwl_handle_aqua(mev,butnum,mouseX,mouseY); 
-	if(fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
-		//swDebugf("onMouse before\n");
+	if(fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 #ifdef STATUSBAR_HUD
 		statusbar_handle_mouse(mouseAction,mouseButton,x,y);
 #else
 		fwl_handle_aqua(mouseAction,mouseButton,x,y); 
 #endif
-		//swDebugf("onMouse after\n");
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::onKey(int keyAction,int keyValue){
-
+DLLFREEWRL_API void dllFreeWRL_onKey(void *fwctx, int keyAction,int keyValue){
 	int kp = keyValue;
 	int ka = keyAction;
-	if(fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
-		//swDebugf("onKey before\n");
+	if(fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		switch(keyAction)
 		{
 		case KEYDOWN:
@@ -259,114 +191,102 @@ void CdllFreeWRL::onKey(int keyAction,int keyValue){
 			fwl_do_keyPress(kp,ka);
 			break;
 		}
-		//swDebugf("onKey after\n");
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::onClose()
+DLLFREEWRL_API void dllFreeWRL_onClose(void *fwctx)
 {
     
 	/* when finished: as of early 2014 dug9 changed the _displayThread so now fwl_doQuit() is asynchronous meaning
 	   it returns here immediately, but it takes a while for libfreewrl to finish parking threads, deleting resources
-	   
-	   */
-	if(fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
-		//swDebugf("onClose before -fwl_doQuitInstance being called\n");
-		//fwl_doQuitInstance();
+   */
+	if(fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		fwl_doQuit();
-		//swDebugf("onClose after\n");
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::print(char *str)
+DLLFREEWRL_API void dllFreeWRL_print(void *fwctx, char *str)
 {
-	if(fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
-		//swDebugf(str);
+	if(fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::onDraw()
+DLLFREEWRL_API void dllFreeWRL_onDraw(void *fwctx)
 {
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		int more = fwl_draw();
 	}
 	fwl_clearCurrentHandle();
 }
 
-int CdllFreeWRL::getUpdatedCursorStyle()
+DLLFREEWRL_API int dllFreeWRL_getUpdatedCursorStyle(void *fwctx)
 {
 	int cstyle = 0;
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		cstyle = getCursorStyle();
 	}
 	fwl_clearCurrentHandle();
 	return cstyle;
 }
-#if !defined(nullptr) 
-#define nullptr (char*)0
+#if !defined(NULL) 
+#define NULL (char*)0
 #endif
-void* CdllFreeWRL::frontenditem_dequeue()
+DLLFREEWRL_API void* dllFreeWRL_frontenditem_dequeue(void *fwctx)
 {
-	void *item = nullptr;
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+	void *item = NULL;
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		item = fwl_frontenditem_dequeue();
 	}
 	fwl_clearCurrentHandle();
 	return item;
 }
-char* CdllFreeWRL::resitem_getURL(void *res)
-{
-	char *url = nullptr;
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+DLLFREEWRL_API char* dllFreeWRL_resitem_getURL(void *fwctx, void *res){
+	char *url = NULL;
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		url = fwl_resitem_getURL(res);
 	}
 	fwl_clearCurrentHandle();
 	return url;
 }
-int CdllFreeWRL::resitem_getStatus(void *res)
-{
+DLLFREEWRL_API int dllFreeWRL_resitem_getStatus(void *fwctx, void *res){
 	int status;
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		status = fwl_resitem_getStatus(res);
 	}
 	fwl_clearCurrentHandle();
 	return status;
 }
-int CdllFreeWRL::resitem_getType(void *res)
-{
+DLLFREEWRL_API int dllFreeWRL_resitem_getType(void *fwctx, void *res){
 	int status;
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		status = fwl_resitem_getType(res);
 	}
 	fwl_clearCurrentHandle();
 	return status;
 }
-void CdllFreeWRL::resitem_enqueuNextMulti(void *res){
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+DLLFREEWRL_API void dllFreeWRL_resitem_enqueuNextMulti(void *fwctx, void *res){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		fwl_resitem_enqueuNextMulti(res);
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::resitem_setLocalPath(void *res, char* path){
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+DLLFREEWRL_API void dllFreeWRL_resitem_setLocalPath(void *fwctx, void *res, char* path){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		fwl_resitem_setLocalPath(res,path);
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::resitem_load(void *res){
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+DLLFREEWRL_API void dllFreeWRL_resitem_load(void *fwctx, void *res){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		if (file2blob(res))
 			fwl_resitem_enqueue(res);
 	}
 	fwl_clearCurrentHandle();
 }
-void CdllFreeWRL::resitem_enqueue(void *res){
-	if (fwl_setCurrentHandle(this->globalcontexthandle, __FILE__, __LINE__)){
+DLLFREEWRL_API void dllFreeWRL_resitem_enqueue(void *fwctx, void *res){
+	if (fwl_setCurrentHandle(fwctx, __FILE__, __LINE__)){
 		fwl_resitem_enqueue(res);
 	}
 	fwl_clearCurrentHandle();
 }
-
-
-
 
