90e5b5f5c99e29ee965e6234b5c86b181c339a59
dug9 <highaspirations@hotmail.com>
2016-07-20 08:01:30 -0600

    improved comments for future Hermite splines

--- freex3d/src/lib/scenegraph/Component_Interpolation.c
+++ freex3d/src/lib/scenegraph/Component_Interpolation.c
@@ -60,13 +60,86 @@
 
 void do_EaseInEaseOut(void *node){
 	// http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/interp.html#EaseInEaseOut
+	
 }
+
+/*
+	Splines via Hermite
+	http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/interp.html#HermiteSplineInterpolation
+	https://en.wikipedia.org/wiki/Cubic_Hermite_spline
+	- you give it a slope and position at each end, and a 0-1 t
+	- you can compute for xyz by doing 3 separate scalar interpolations one for each coordinate
+	- or to save FLOPS (floating point ops) you can do a template algo like we did for Followers
+
+	if linear interp looks like this:
+	fraction s = fraction_interp(t,t0,t1);
+	answer = linear_interp(s,key0,val0,key1,val1)
+	
+	Then spline interp would look like this:
+	fraction s = fraction_interp(t,t0,t1);
+	answer = spline_interp(s,key0,val0,vel0,key1,val1,vel1)  
+	- where vel is velocity or more generally slope/first-derivitive of value at key. 
+	-- and first derivitive would be with respect to key ie d(Value)/d(key) evaluated at key
+	-- and according to specs you calculate defaults if user doesn't supply
+	- and in general {answer, val*, vel*} are vectors or types ie SFVec3f, SFRotation, SFfloat
+
+*/
+
 void do_SplinePositionInterpolator(void *node){
-	// http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/interp.html#SplinePositionInterpolator
+	//	http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/interp.html#SplinePositionInterpolator
+
 }
 void do_SplinePositionInterpolator2D(void *node){
 }
 void do_SplineScalarInterpolator(void *node){
+	// SplineScalarInterpolator - store final value in px->value_changed 
+	//  - body of function copied from ScalarInterpolator and node cast to SplineScalarInterpolator
+	struct X3D_SplineScalarInterpolator *px;
+	int kin, kvin;
+	float *kVs;
+	int counter;
+
+	if (!node) return;
+	px = (struct X3D_SplineScalarInterpolator *) node;
+	kin = px->key.n;
+	kvin = px->keyValue.n;
+	kVs = px->keyValue.p;
+
+	MARK_EVENT (node, offsetof (struct X3D_SplineScalarInterpolator, value_changed));
+
+	/* make sure we have the keys and keyValues */
+	if ((kvin == 0) || (kin == 0)) {
+		px->value_changed = (float) 0.0;
+		return;
+	}
+	if (kin>kvin) kin=kvin; /* means we don't use whole of keyValue, but... */
+
+	#ifdef SEVERBOSE
+		printf ("ScalarInterpolator, kin %d kvin %d, vc %f\n",kin,kvin,px->value_changed);
+	#endif
+
+	/* set_fraction less than or greater than keys */
+	if (px->set_fraction <= px->key.p[0]) {
+		 px->value_changed = kVs[0];
+	} else if (px->set_fraction >= px->key.p[kin-1]) {
+		 px->value_changed = kVs[kvin-1];
+	} else {
+		/* have to go through and find the key before */
+		counter=find_key(kin,(float)(px->set_fraction),px->key.p);
+		// INTERPOLATION FUNCTION - change this from linear to spline
+		// fraction s = fraction_interp(t,t0,t1);
+		// answer = linear_interp(s,key0,val0,key1,val1)
+		// answer = spline_interp(s,key0,val0,vel0,key1,val1,vel1)  
+		//  where vel is velocity or more generally slope/first-derivitive of value at key. 
+		//  First derivitive would be with respect to key ie d(Value)/d(key) evaluated at key
+		// in general answer, val*, vel* are vectors or types.
+		px->value_changed =
+			(px->set_fraction - px->key.p[counter-1]) /
+			(px->key.p[counter] - px->key.p[counter-1]) *
+			(kVs[counter] - kVs[counter-1]) +
+			kVs[counter-1];
+	}
+
 }
 void do_SquadOrientationInterpolator(void *node){
 	// http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/interp.html#SquadOrientationInterpolator
@@ -86,7 +159,9 @@ void do_SquadOrientationInterpolator(void *node){
 	//		and qi+1 means q[i+1] and qi-1 means q[i-1] ie that's an indexer, not a quat + scalar
 	//	p. 15 shows log(q) if q=[cost,sint*v] then 
 	//			log(q) = [0, sint*v] (non-unit)
-	//			exp(q) = [cost, sint*v] (puts cost back in scalar part, inverting log)
+	//			exp(q) = [cost, sint*v] (puts cos(t) back in scalar part, inverting log)
+	//						remember from trig cos**2 + sin**2 = 1, or cos = sqrt(1 - sin**2)
+	//						can probably take sine from sint*v ie sint = veclength(sint*v)
 	//	July 19, 2016 - code below copied from orientationInterpolator and node caste changed, but no other changes
 	//  - feel free to implement squad in here
 
