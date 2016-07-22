a0db71e63f92b580223435820f68a9d1458e3d84
dug9 <highaspirations@hotmail.com>
2016-07-22 10:22:25 -0600

    comment on mpeg1

--- freex3d/src/lib/scenegraph/MPEG_Utils.c
+++ freex3d/src/lib/scenegraph/MPEG_Utils.c
@@ -28,4 +28,32 @@
 /* NOTE: we have to re-implement the loading of movie textures; the code in here was a decade old and did not
 keep up with "the times". Check for ifdef HAVE_TO_REIMPLEMENT_MOVIETEXTURES in the code */
 
+/* July 2016 note:
+	http://www.web3d.org/documents/specifications/19775-1/V3.3/Part01/components/texturing.html#MovieTexture
+	- specs say to support MPEG1
+	http://www.web3d.org/x3d/content/examples/ConformanceNist/
+	- see Movie texture example with VTS.mpg
 
+	There are still patents and licensing issues with recent mp4 and even mpeg-2 I've heard.
+	MPEG1 - any patents have expired
+	Goal: a generic interface we can use to wrap 3 implementations:
+		1. stub
+		2. option: old-fashioned cross-platform mpeg1 c code, as fallback/default
+		3. platform-supplied/platform-specific video API/libraries 
+	and to use both audio and video streams, with audio compatible with Sound node api needs.
+
+	Links > 
+	2. old fashioned mpeg1
+	Mpeg-1 libs
+	https://en.wikipedia.org/wiki/MPEG-1
+	- See source code link at bottom, reference implementation
+	- which has an ISO license which dug9 reads to mean: 
+	--- use for simulation of what electronic devices will do, presumably by electronics companies
+	--- (but not intendended / foreseen as a software library product, uncertain if allowed)
+	- it includes audio and video
+	http://www.gerg.ca/software/mpeglib/
+	- Mpeg1 - hack of berkley mpeg1 code, no separate license on hacks show
+	http://www.uow.edu.au/~nabg/MPEG/mpeg1.html
+	- Mpeg1 explanation with hacked Berkley code and license: berkley + do-what-you-want-with-hacked-code 
+
+*/
